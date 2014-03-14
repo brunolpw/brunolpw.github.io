ad061bfe7cce7df50de810548d7a4c492578d429
Bruno <brunolpw@outlook.com>
2014-03-14 04:01:02 -0300

    Update hellogit.php

--- hellogit.php
+++ hellogit.php
@@ -7,6 +7,6 @@
 	<body>
 	<!--https://github.com/brunolpw/brunolpw.github.io.git-->
 		<h1>Hello World.</h1>
-		<? php echo "Hello GitHub PHP."; ?>
+		<? php echo "<p>Hello GitHub PHP.</p>"; ?>
 	</body>
 </html>

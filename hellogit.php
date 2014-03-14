4bce0f6238ab55286d2d594b544bc3da7b8d5598
Bruno <brunolpw@outlook.com>
2014-03-14 03:57:01 -0300

    php

---
+++ hellogit.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello my git</title>
+	</head>
+
+	<body>
+	<!--https://github.com/brunolpw/brunolpw.github.io.git-->
+		<h1>Hello World.</h1>
+		<? php echo "Hello GitHub PHP."; ?>
+	</body>
+</html>

14b4cc939cda7f4a0010195bc19cc7f3fd863f84
Bruno <brunolpw@outlook.com>
2014-06-05 00:19:02 -0300

    Organização
    Códigos antigos e de testes movidos para uma pasta especifica, dando
    lugar ao código novo e definitivo.

---
+++ js/shBrushAll.js
@@ -0,0 +1,22 @@
+﻿
+/*************************************
+ * Autor: Bruno La Porta Welausen    *
+ * Data: 04/06/2014                  *
+ * Versão: 1.0.0                     *
+ *                                   *
+ *************************************/
+ 
+ /*
+  *		Este código tem por objetivo de carregar todos os arquivos shBrush.js para evitar que o usuario tenha de ficar carregando
+  * e selecionando cada um dos arquivos que deseja usar, sendo assim facilitando para o usuário do sistema, ficando a cargo de
+  * carregar unicamente este arquivo, ou se assim desejar carregar cada um individualmente.
+  * */
+
+  include('syntaxhighlighter_3.0.83/scripts/shBrushCpp.js');
+  include('syntaxhighlighter_3.0.83/scripts/shBrushCSharp.js');
+  include('syntaxhighlighter_3.0.83/scripts/shBrushJScript.js');
+
+ /* document.write('<script type="text/javascript" src="syntaxhighlighter_3.0.83/scripts/shBrushCpp.js"></script>');
+  document.write('<script type="text/javascript" src="syntaxhighlighter_3.0.83/scripts/shBrushCSharp.js"></script>');
+  document.write('<script type="text/javascript" src="syntaxhighlighter_3.0.83/scripts/shBrushJScript.js"></script>');
+*/
